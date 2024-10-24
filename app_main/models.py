e446c74d49d6a1eee6fde874a3070273b5fd5242
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2024-10-25 03:10:57 +0500

    Update Project model to store photos in project-photos folder

--- app_main/models.py
+++ app_main/models.py
@@ -9,7 +9,7 @@ class Project(models.Model):
     title = models.CharField(max_length=255, unique=True)
     link = models.URLField(max_length=255)
     description = models.TextField(null=True, blank=True)
-    photo = models.ImageField(upload_to='thedevu101-media/projects')
+    photo = models.ImageField(upload_to='project-photos/')
 
     id = models.UUIDField(default=uuid.uuid4, editable=False,
                           primary_key=True, unique=True)
